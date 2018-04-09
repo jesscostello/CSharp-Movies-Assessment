692aa77cebeeb6dfd5d5a023385ca888c7f46069
Jess Costello <jessica.a.costello@gmail.com>
2018-04-10 10:10:39 +1200

    First Upload

--- CSharp Movies Assessment/Form1.Designer.cs
+++ CSharp Movies Assessment/Form1.Designer.cs
@@ -28,13 +28,208 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabCustomers = new System.Windows.Forms.TabPage();
+            this.tabMovies = new System.Windows.Forms.TabPage();
+            this.tabRented = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvCustomers = new System.Windows.Forms.DataGridView();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.lblFN = new System.Windows.Forms.Label();
+            this.lblLN = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabCustomers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabCustomers);
+            this.tabControl1.Controls.Add(this.tabMovies);
+            this.tabControl1.Controls.Add(this.tabRented);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(885, 412);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabCustomers
+            // 
+            this.tabCustomers.Controls.Add(this.splitContainer1);
+            this.tabCustomers.Location = new System.Drawing.Point(4, 22);
+            this.tabCustomers.Name = "tabCustomers";
+            this.tabCustomers.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCustomers.Size = new System.Drawing.Size(877, 386);
+            this.tabCustomers.TabIndex = 0;
+            this.tabCustomers.Text = "Customers";
+            this.tabCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tabMovies
+            // 
+            this.tabMovies.Location = new System.Drawing.Point(4, 22);
+            this.tabMovies.Name = "tabMovies";
+            this.tabMovies.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMovies.Size = new System.Drawing.Size(877, 386);
+            this.tabMovies.TabIndex = 1;
+            this.tabMovies.Text = "Movies";
+            this.tabMovies.UseVisualStyleBackColor = true;
+            // 
+            // tabRented
+            // 
+            this.tabRented.Location = new System.Drawing.Point(4, 22);
+            this.tabRented.Name = "tabRented";
+            this.tabRented.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRented.Size = new System.Drawing.Size(877, 386);
+            this.tabRented.TabIndex = 2;
+            this.tabRented.Text = "Rented Movies";
+            this.tabRented.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblPhone);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPhone);
+            this.splitContainer1.Panel1.Controls.Add(this.lblAddress);
+            this.splitContainer1.Panel1.Controls.Add(this.txtAddress);
+            this.splitContainer1.Panel1.Controls.Add(this.lblLN);
+            this.splitContainer1.Panel1.Controls.Add(this.txtLastName);
+            this.splitContainer1.Panel1.Controls.Add(this.lblFN);
+            this.splitContainer1.Panel1.Controls.Add(this.txtFirstName);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dgvCustomers);
+            this.splitContainer1.Size = new System.Drawing.Size(871, 380);
+            this.splitContainer1.SplitterDistance = 150;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // dgvCustomers
+            // 
+            this.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvCustomers.Location = new System.Drawing.Point(0, 0);
+            this.dgvCustomers.Name = "dgvCustomers";
+            this.dgvCustomers.Size = new System.Drawing.Size(717, 380);
+            this.dgvCustomers.TabIndex = 0;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(3, 27);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(144, 20);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // lblFN
+            // 
+            this.lblFN.AutoSize = true;
+            this.lblFN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFN.Location = new System.Drawing.Point(4, 8);
+            this.lblFN.Name = "lblFN";
+            this.lblFN.Size = new System.Drawing.Size(71, 13);
+            this.lblFN.TabIndex = 1;
+            this.lblFN.Text = "First Name:";
+            // 
+            // lblLN
+            // 
+            this.lblLN.AutoSize = true;
+            this.lblLN.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLN.Location = new System.Drawing.Point(4, 50);
+            this.lblLN.Name = "lblLN";
+            this.lblLN.Size = new System.Drawing.Size(71, 13);
+            this.lblLN.TabIndex = 3;
+            this.lblLN.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(3, 69);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(144, 20);
+            this.txtLastName.TabIndex = 2;
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(4, 92);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(56, 13);
+            this.lblAddress.TabIndex = 5;
+            this.lblAddress.Text = "Address:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(3, 111);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(144, 20);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPhone.Location = new System.Drawing.Point(4, 134);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(94, 13);
+            this.lblPhone.TabIndex = 7;
+            this.lblPhone.Text = "Phone Number:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(3, 153);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(144, 20);
+            this.txtPhone.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(910, 521);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Movies Database";
+            this.tabControl1.ResumeLayout(false);
+            this.tabCustomers.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomers)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabCustomers;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label lblLN;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label lblFN;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.DataGridView dgvCustomers;
+        private System.Windows.Forms.TabPage tabMovies;
+        private System.Windows.Forms.TabPage tabRented;
     }
 }
 
